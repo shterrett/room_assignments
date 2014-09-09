d38cb2d4283d3c2e7c70b44003fca61ef78b686a
Stuart Terrett <shterrett@gmail.com>
2014-09-09 06:51:27 -0400

    Command line args + print to stdout

--- ReadCsv.hs
+++ ReadCsv.hs
@@ -8,6 +8,7 @@ import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import qualified Data.Vector as V
 import RoomAssignments
+import System.Environment (getArgs)
 
 instance FromNamedRecord Event where
     parseNamedRecord r = Event <$> r .: "name" <*>
@@ -20,8 +21,8 @@ instance FromField Time where
     parseField str =
       let time = splitTimeField str in
         case time of
-          h:m:s -> pure (Time h m)
-          otherwise -> empty
+          h:m:_ -> pure (Time h m)
+          _ -> empty
 
 splitTimeField :: Field -> [Integer]
 splitTimeField str = map (\s -> read s :: Integer) $ splitOn ":" (BC.unpack str)
@@ -41,20 +42,21 @@ formatRoom :: Room -> EventList -> String -> String
 formatRoom room events text = text ++ (foldl (formatEvent room) ""  events)
 
 formatEvent :: Room -> String -> Event -> String
-formatEvent room lines event = (name event) ++
+formatEvent room evLns event = (name event) ++
                                "," ++ (show (startTime event)) ++
                                "," ++ (show (endTime event)) ++
                                "," ++ (show (attending event)) ++
                                "," ++ (roomId room) ++
-                               "\n" ++ lines
+                               "\n" ++ evLns
 
 formatSchedule :: Schedule -> String
 formatSchedule s = M.foldWithKey formatRoom "Event,Start,End,Attending,Room\n" s
 
 main :: IO ()
 main = do
-    eventData <- BL.readFile "./test/events.csv"
-    roomData <- BL.readFile "./test/rooms.csv"
+    (eventFile:roomFile:_) <- getArgs
+    eventData <- BL.readFile eventFile
+    roomData <- BL.readFile roomFile
     let eventList = case decodeByName eventData of
                       Left _ -> []
                       Right (_, v) -> V.toList v
@@ -63,4 +65,4 @@ main = do
                       Left _ -> []
                       Right (_, v) -> V.toList v
 
-    writeFile "./test/schedule.csv" (formatSchedule $ scheduleRoomsForEvents eventList roomList)
+    putStrLn (formatSchedule $ scheduleRoomsForEvents eventList roomList)
